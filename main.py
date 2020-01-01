ae7e5d7e2bdc9e344653692fb57748ca00b82232
Julio Pinto Coelho <juliopcrj@gmail.com>
2020-01-01 17:27:47 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import os, sys
+import argparse
+
+def main(arguments):
+    if arguments[0] == "update":
+        pass
+    if arguments[0] == "add":
+        pass
+    if arguments[0] == "del":
+        pass
+    if arguments[0] == "resend":
+        pass
+
+   
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
